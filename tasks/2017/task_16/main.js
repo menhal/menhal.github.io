85fc63dab9263616c436afa50e2da39830a72d5c
menghao@dufetech.com <una5gu8w>
2017-02-27 12:53:58 +0800

    任务16

--- tasks/2017/task_16/main.js
+++ tasks/2017/task_16/main.js
@@ -38,13 +38,10 @@ function drawHue(){
     var canvas = domHue.getContext("2d")
     for(var y=0; y<400; y++){
         canvas.strokeStyle = hslToHex(y, 100, 50);
-        //console.log(canvas.fillStyle)
         canvas.beginPath();
         canvas.moveTo(0, y);
         canvas.lineTo(20, y);
         canvas.stroke();
-
-        //canvas.fillRect(i,j,1,1);
     }
 }
 
@@ -99,56 +96,4 @@ function hslToRgb(h, s, l){
 function hslToHex(h, s, l) {
     var rgb = hslToRgb(h, s, l);
     return ("rgb("+rgb[0]+","+rgb[1]+","+rgb[2]+")").colorHex();
-}
-
-function rgbToHsl(r, g, b){
-    r /= 255, g /= 255, b /= 255;
-    var max = Math.max(r, g, b), min = Math.min(r, g, b);
-    var h, s, l = (max + min) / 2;
-
-    if(max == min){
-        h = s = 0; // achromatic
-    }else{
-        var d = max - min;
-        s = l > 0.5 ? d / (2 - max - min) : d / (max + min);
-        switch(max){
-            case r: h = (g - b) / d + (g < b ? 6 : 0); break;
-            case g: h = (b - r) / d + 2; break;
-            case b: h = (r - g) / d + 4; break;
-        }
-        h /= 6;
-    }
-
-    return [h, s, l];
-}
-
-String.prototype.hsl2Rgb = function() {
-    var hslReg = /^hsl\(.+?\)$/gi;
-    var r, g, b;
-    var o = this.replace(/hsl\(|\)|\s/gi, '').split(',');
-
-    h = parseInt(o[0]) / 360;
-    s = parseInt(o[1]) / 100;
-    l = parseInt(o[2]) / 100;
-
-    if(hslReg.test(this)) {
-        if (s == 0) {
-            r = g = b = l; // achromatic
-        } else {
-            var hue2rgb = function hue2rgb(p, q, t) {
-                if (t < 0) t += 1;
-                if (t > 1) t -= 1;
-                if (t < 1 / 6) return p + (q - p) * 6 * t;
-                if (t < 1 / 2) return q;
-                if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-                return p;
-            }
-            var q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-            var p = 2 * l - q;
-            r = hue2rgb(p, q, h + 1 / 3);
-            g = hue2rgb(p, q, h);
-            b = hue2rgb(p, q, h - 1 / 3);
-        }
-        return 'rgb(' + Math.round(r * 255) + ', ' + Math.round(g * 255) + ', ' + Math.round(b * 255) + ')';
-    }
 }
